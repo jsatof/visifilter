3e183e89e16ddf2bcfc65f26cb0b5cf5d27472fb
James Ferrarelli <ferrarellijames@gmail.com>
2024-12-13 23:22:07 -0500

    init
    compiled a base project that can build on linux and windows
    configured with includes to the audio and gui libraries

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// TODO: move implementation definition
+#define MINIAUDIO_IMPLEMENTATION
+#include <miniaudio/miniaudio.h>
+
+// TODO: move implementation definition
+#define CLAY_IMPLEMENTATION
+#include <clay/clay.h>
+
+int main() {
+	std::cout << "clay + miniaudio\n";
+}
